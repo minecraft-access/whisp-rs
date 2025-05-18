baab9e951dd54d8efe7531f0a86f911d68f7539c
Elijah Massey <emassey0135@gmail.com>
2025-05-17 19:34:36 -0500

    Move audio playback with Rodio out of eSpeak NG backend into main class to support all backends with no built-in audio playback

--- src/speech.rs
+++ src/speech.rs
@@ -2,6 +2,7 @@ use std::cell::{OnceCell,RefCell};
 use std::collections::HashMap;
 use std::sync::{mpsc,Mutex,OnceLock};
 use std::thread;
+use rodio::{buffer::SamplesBuffer,OutputStream,Sink};
 use crate::speech_synthesizer::*;
 use crate::espeak_ng::EspeakNg;
 #[cfg(windows)] use crate::sapi::Sapi;
@@ -9,7 +10,9 @@ use crate::espeak_ng::EspeakNg;
 #[cfg(target_os = "macos")] use crate::av_speech_synthesizer::AvSpeechSynthesizer;
 thread_local! {
   static SYNTHESIZERS: RefCell<HashMap<String, Box<dyn SpeechSynthesizer>>> = RefCell::new(HashMap::new());
+  static OUTPUT_STREAM: RefCell<Option<OutputStream>> = RefCell::new(None);
 }
+static SINK: OnceLock<Sink> = OnceLock::new();
 enum Operation {
   ListVoices,
   SpeakToAudioData(String, String, String, Option<u8>, Option<u8>, Option<u8>, String),
@@ -31,6 +34,10 @@ pub fn initialize() -> Result<(), SpeechError> {
   let (result_tx, result_rx) = mpsc::channel();
   RESULT_RX.lock().unwrap().set(result_rx).unwrap();
   thread::spawn(move || {
+    let (output_stream, output_stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&output_stream_handle).unwrap();
+    OUTPUT_STREAM.set(Some(output_stream));
+    let _result = SINK.set(sink);
     let result = SYNTHESIZERS.with_borrow_mut(|synthesizers| {
       let espeak_ng = EspeakNg::new()?;
       synthesizers.insert(espeak_ng.data().name, Box::new(espeak_ng));
@@ -99,7 +106,24 @@ fn internal_speak_to_audio_output(synthesizer: &str, voice: &str, language: &str
     match synthesizers.get(synthesizer) {
       None => return Err(SpeechError { message: "Unknown synthesizer".to_owned() }),
       Some(synthesizer) => match synthesizer.as_to_audio_output() {
-        None => return Err(SpeechError { message: "Synthesizer does not support speaking to audio output".to_owned() }),
+        None => {
+          match synthesizer.as_to_audio_data() {
+            None => return Err(SpeechError { message: "Synthesizer does not support playing or returning audio".to_owned() }),
+            Some(synthesizer) => {
+              let result = synthesizer.speak(voice, language, rate, volume, pitch, text)?;
+              let buffer = result.pcm
+                .chunks_exact(2)
+                .map(|chunk| i16::from_le_bytes([chunk[0], chunk[1]]))
+                .collect::<Vec<i16>>();
+              let source = SamplesBuffer::new(1, result.sample_rate, buffer);
+              if interrupt {
+                SINK.get().unwrap().stop();
+              };
+              SINK.get().unwrap().append(source);
+              Ok(())
+            }
+          }
+        },
         Some(synthesizer) => synthesizer.speak(voice, language, rate, volume, pitch, text, interrupt)
       }
     }
@@ -117,7 +141,10 @@ fn internal_stop_speech(synthesizer: &str) -> Result<(), SpeechError> {
     match synthesizers.get(synthesizer) {
       None => return Err(SpeechError { message: "Unknown synthesizer".to_owned() }),
       Some(synthesizer) => match synthesizer.as_to_audio_output() {
-        None => return Err(SpeechError { message: "Synthesizer does not support speaking to audio output".to_owned() }),
+        None => {
+          SINK.get().unwrap().stop();
+          Ok(())
+        },
         Some(synthesizer) => synthesizer.stop_speech()
       }
     }
