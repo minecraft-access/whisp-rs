9e57cbb93e98ccf49260cae2e58e491a9193fa5f
Elijah Massey <emassey0135@gmail.com>
2025-05-22 21:12:16 -0500

    Fix Speech Dispatcher backend

--- src/speech_dispatcher.rs
+++ src/speech_dispatcher.rs
@@ -8,12 +8,24 @@ pub struct SpeechDispatcher {
 }
 impl SpeechSynthesizer for SpeechDispatcher {
   fn new() -> Result<Self, SpeechError> {
-    let mut client = fifo::Builder::new().build()?;
+    let mut client = fifo::Builder::new()
+      .build()
+      .map_err(SpeechError::into_unknown)?;
     client
-      .set_client_name(ClientName::new("", "audio-navigation-tts"))?
-      .check_client_name_set()?;
-    let default_output_module = client.get_output_module()?.receive_string(OK_GET)?;
-    let default_language = client.get_language()?.receive_string(OK_GET)?;
+      .set_client_name(ClientName::new("", "audio-navigation-tts"))
+      .map_err(SpeechError::into_unknown)?
+      .check_client_name_set()
+      .map_err(SpeechError::into_unknown)?;
+    let default_output_module = client
+      .get_output_module()
+      .map_err(SpeechError::into_unknown)?
+      .receive_string(OK_GET)
+      .map_err(SpeechError::into_unknown)?;
+    let default_language = client
+      .get_language()
+      .map_err(SpeechError::into_unknown)?
+      .receive_string(OK_GET)
+      .map_err(SpeechError::into_unknown)?;
     Ok(SpeechDispatcher {
       default_output_module,
       default_language,
@@ -31,8 +43,10 @@ impl SpeechSynthesizer for SpeechDispatcher {
   fn list_voices(&self) -> Result<Vec<Voice>, SpeechError> {
     let mut client = self.client.borrow_mut();
     let voices = client
-      .list_output_modules()?
-      .receive_lines(OK_OUTPUT_MODULES_LIST_SENT)?
+      .list_output_modules()
+      .map_err(SpeechError::into_unknown)?
+      .receive_lines(OK_OUTPUT_MODULES_LIST_SENT)
+      .map_err(SpeechError::into_unknown)?
       .into_iter()
       .flat_map(|module| {
         client
@@ -58,7 +72,7 @@ impl SpeechSynthesizer for SpeechDispatcher {
               priority: 1,
             })
           })
-          .collect::<Result<Vec<Voice>, SpeechError>>()
+          .collect::<Result<Vec<Voice>, anyhow::Error>>()
       })
       .flatten()
       .collect();
@@ -90,9 +104,7 @@ impl SpeechSynthesizerToAudioOutput for SpeechDispatcher {
           .list_voices()?
           .into_iter()
           .find(|voice| voice.languages.iter().any(|name| name == language))
-          .ok_or(SpeechError {
-            message: "Voice not found".to_owned(),
-          })?
+          .ok_or(SpeechError::into_language_not_found(language))?
           .name,
       ),
     };
@@ -100,61 +112,163 @@ impl SpeechSynthesizerToAudioOutput for SpeechDispatcher {
     match voice {
       None => {
         client
-          .set_output_module(ClientScope::Current, &self.default_output_module)?
-          .check_status(OK_OUTPUT_MODULE_SET)?;
+          .set_output_module(ClientScope::Current, &self.default_output_module)
+          .map_err(|err| {
+            SpeechError::into_speak_failed(&self.data().name, &self.default_output_module, err)
+          })?
+          .check_status(OK_OUTPUT_MODULE_SET)
+          .map_err(|err| {
+            SpeechError::into_speak_failed(&self.data().name, &self.default_output_module, err)
+          })?;
         client
-          .set_language(ClientScope::Current, &self.default_language)?
-          .check_status(OK_LANGUAGE_SET)?;
+          .set_language(ClientScope::Current, &self.default_language)
+          .map_err(|err| {
+            SpeechError::into_speak_failed(&self.data().name, &self.default_language, err)
+          })?
+          .check_status(OK_LANGUAGE_SET)
+          .map_err(|err| {
+            SpeechError::into_speak_failed(&self.data().name, &self.default_language, err)
+          })?;
       }
-      Some(voice) => {
+      Some(ref voice) => {
         let mut split = voice.split('/');
         let output_module = split.next().unwrap();
         let voice = split.next().unwrap();
         client
-          .set_output_module(ClientScope::Current, output_module)?
-          .check_status(OK_OUTPUT_MODULE_SET)?;
+          .set_output_module(ClientScope::Current, output_module)
+          .map_err(|err| SpeechError::into_speak_failed(&self.data().name, output_module, err))?
+          .check_status(OK_OUTPUT_MODULE_SET)
+          .map_err(|err| SpeechError::into_speak_failed(&self.data().name, output_module, err))?;
         client
-          .set_synthesis_voice(ClientScope::Current, voice)?
-          .check_status(OK_VOICE_SET)?;
+          .set_synthesis_voice(ClientScope::Current, voice)
+          .map_err(|err| SpeechError::into_speak_failed(&self.data().name, voice, err))?
+          .check_status(OK_VOICE_SET)
+          .map_err(|err| SpeechError::into_speak_failed(&self.data().name, voice, err))?;
       }
     };
     let rate = rate.unwrap_or(50) as i8;
     let rate = (rate * 2) - 100;
     client
-      .set_rate(ClientScope::Current, rate)?
-      .check_status(OK_RATE_SET)?;
+      .set_rate(ClientScope::Current, rate)
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?
+      .check_status(OK_RATE_SET)
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?;
     let pitch = pitch.unwrap_or(50) as i8;
     let pitch = (pitch * 2) - 100;
     client
-      .set_pitch(ClientScope::Current, pitch)?
-      .check_status(OK_PITCH_SET)?;
+      .set_pitch(ClientScope::Current, pitch)
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?
+      .check_status(OK_PITCH_SET)
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?;
     let volume = volume.unwrap_or(50) as i8;
     let volume = (volume * 2) - 100;
     client
-      .set_volume(ClientScope::Current, volume)?
-      .check_status(OK_VOLUME_SET)?;
+      .set_volume(ClientScope::Current, volume)
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?
+      .check_status(OK_VOLUME_SET)
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?;
     if interrupt {
       client
-        .cancel(MessageScope::Last)?
-        .check_status(OK_CANCELED)?;
+        .cancel(MessageScope::Last)
+        .map_err(|err| {
+          SpeechError::into_speak_failed(
+            &self.data().name,
+            voice.as_deref().unwrap_or(&self.default_output_module),
+            err,
+          )
+        })?
+        .check_status(OK_CANCELED)
+        .map_err(|err| {
+          SpeechError::into_speak_failed(
+            &self.data().name,
+            voice.as_deref().unwrap_or(&self.default_output_module),
+            err,
+          )
+        })?;
     };
     let lines = text
       .lines()
       .map(|line| line.to_owned())
       .collect::<Vec<String>>();
     client
-      .speak()?
-      .check_receiving_data()?
-      .send_lines(&lines)?
-      .receive_message_id()?;
+      .speak()
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?
+      .check_receiving_data()
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?
+      .send_lines(&lines)
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?
+      .receive_message_id()
+      .map_err(|err| {
+        SpeechError::into_speak_failed(
+          &self.data().name,
+          voice.as_deref().unwrap_or(&self.default_output_module),
+          err,
+        )
+      })?;
     Ok(())
   }
   fn stop_speech(&self) -> std::result::Result<(), SpeechError> {
     self
       .client
       .borrow_mut()
-      .cancel(MessageScope::Last)?
-      .check_status(OK_CANCELED)?;
+      .cancel(MessageScope::Last)
+      .map_err(|err| SpeechError::into_stop_speech_failed(&self.data().name, err))?
+      .check_status(OK_CANCELED)
+      .map_err(|err| SpeechError::into_stop_speech_failed(&self.data().name, err))?;
     Ok(())
   }
 }
