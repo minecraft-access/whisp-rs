957d39bc0e3d072b534a26cde8bb6d59d2c5ed7d
Elijah Massey <emassey0135@gmail.com>
2025-05-24 21:26:42 -0500

    Add the rest of the JNI functions

--- src/jni.rs
+++ src/jni.rs
@@ -85,7 +85,45 @@ pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_listVoices<'local>(
   array
 }
 #[no_mangle]
-pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_speak<'local>(
+pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_listBrailleBackends<'local>(
+  mut env: JNIEnv<'local>,
+  _class: JClass<'local>,
+) -> JObjectArray<'local> {
+  let backends = list_braille_backends().unwrap();
+  let braille_backend_class = env.find_class("org/mcaccess/whisprs/BrailleBackend").unwrap();
+  let backends = backends
+    .into_iter()
+    .map(|backend| {
+      let name = env.new_string(&backend.name).unwrap();
+      let priority = backend.priority as i8;
+      env
+        .new_object(
+          &braille_backend_class,
+          "(Ljava/lang/String;B)V",
+          &[
+            JValue::Object(&name),
+            JValue::Byte(priority),
+          ],
+        )
+        .unwrap()
+    })
+    .collect::<Vec<JObject>>();
+  let array = env
+    .new_object_array(
+      backends.len().try_into().unwrap(),
+      &braille_backend_class,
+      JObject::null(),
+    )
+    .unwrap();
+  for (index, backend) in backends.into_iter().enumerate() {
+    env
+      .set_object_array_element(&array, index.try_into().unwrap(), backend)
+      .unwrap();
+  }
+  array
+}
+#[no_mangle]
+pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_speakToAudioData<'local>(
   mut env: JNIEnv<'local>,
   _class: JClass<'local>,
   synthesizer: JString<'local>,
@@ -169,4 +207,206 @@ pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_speak<'local>(
       ],
     )
     .unwrap()
+}
+#[no_mangle]
+pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_speakToAudioOutput<'local>(
+  mut env: JNIEnv<'local>,
+  _class: JClass<'local>,
+  synthesizer: JString<'local>,
+  voice: JString<'local>,
+  language: JString<'local>,
+  rate: JObject<'local>,
+  volume: JObject<'local>,
+  pitch: JObject<'local>,
+  text: JString<'local>,
+  interrupt: jboolean,
+) {
+  let null = JObject::null();
+  let synthesizer: Option<String> = if env.is_same_object(&synthesizer, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&synthesizer).unwrap().into())
+  };
+  let voice: Option<String> = if env.is_same_object(&voice, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&voice).unwrap().into())
+  };
+  let language: Option<String> = if env.is_same_object(&language, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&language).unwrap().into())
+  };
+  let rate: Option<u8> = if env.is_same_object(&rate, &null).unwrap() {
+    None
+  } else {
+    Some(
+      env
+        .call_method(&rate, "byteValue", "()B", &[])
+        .unwrap()
+        .b()
+        .unwrap() as u8,
+    )
+  };
+  let volume: Option<u8> = if env.is_same_object(&volume, &null).unwrap() {
+    None
+  } else {
+    Some(
+      env
+        .call_method(&volume, "byteValue", "()B", &[])
+        .unwrap()
+        .b()
+        .unwrap() as u8,
+    )
+  };
+  let pitch: Option<u8> = if env.is_same_object(&pitch, &null).unwrap() {
+    None
+  } else {
+    Some(
+      env
+        .call_method(&pitch, "byteValue", "()B", &[])
+        .unwrap()
+        .b()
+        .unwrap() as u8,
+    )
+  };
+  let text: String = env.get_string(&text).unwrap().into();
+  let interrupt: bool = match interrupt {
+    JNI_FALSE => false,
+    _ => true,
+  };
+  speak_to_audio_output(
+    synthesizer.as_deref(),
+    voice.as_deref(),
+    language.as_deref(),
+    rate,
+    volume,
+    pitch,
+    &text,
+    interrupt,
+  )
+  .unwrap();
+}
+#[no_mangle]
+pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_stopSpeech<'local>(
+  mut env: JNIEnv<'local>,
+  _class: JClass<'local>,
+  synthesizer: JString<'local>,
+) {
+  let null = JObject::null();
+  let synthesizer: Option<String> = if env.is_same_object(&synthesizer, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&synthesizer).unwrap().into())
+  };
+  stop_speech(
+    synthesizer.as_deref(),
+  )
+  .unwrap();
+}
+#[no_mangle]
+pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_braille<'local>(
+  mut env: JNIEnv<'local>,
+  _class: JClass<'local>,
+  backend: JString<'local>,
+  text: JString<'local>,
+) {
+  let null = JObject::null();
+  let backend: Option<String> = if env.is_same_object(&backend, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&backend).unwrap().into())
+  };
+  let text: String = env.get_string(&text).unwrap().into();
+  braille(
+    backend.as_deref(),
+    &text,
+  )
+  .unwrap();
+}
+#[no_mangle]
+pub extern "system" fn Java_org_mcaccess_whisprs_Whisprs_output<'local>(
+  mut env: JNIEnv<'local>,
+  _class: JClass<'local>,
+  synthesizer: JString<'local>,
+  voice: JString<'local>,
+  language: JString<'local>,
+  rate: JObject<'local>,
+  volume: JObject<'local>,
+  pitch: JObject<'local>,
+  braille_backend: JString<'local>,
+  text: JString<'local>,
+  interrupt: jboolean,
+) {
+  let null = JObject::null();
+  let synthesizer: Option<String> = if env.is_same_object(&synthesizer, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&synthesizer).unwrap().into())
+  };
+  let voice: Option<String> = if env.is_same_object(&voice, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&voice).unwrap().into())
+  };
+  let language: Option<String> = if env.is_same_object(&language, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&language).unwrap().into())
+  };
+  let rate: Option<u8> = if env.is_same_object(&rate, &null).unwrap() {
+    None
+  } else {
+    Some(
+      env
+        .call_method(&rate, "byteValue", "()B", &[])
+        .unwrap()
+        .b()
+        .unwrap() as u8,
+    )
+  };
+  let volume: Option<u8> = if env.is_same_object(&volume, &null).unwrap() {
+    None
+  } else {
+    Some(
+      env
+        .call_method(&volume, "byteValue", "()B", &[])
+        .unwrap()
+        .b()
+        .unwrap() as u8,
+    )
+  };
+  let pitch: Option<u8> = if env.is_same_object(&pitch, &null).unwrap() {
+    None
+  } else {
+    Some(
+      env
+        .call_method(&pitch, "byteValue", "()B", &[])
+        .unwrap()
+        .b()
+        .unwrap() as u8,
+    )
+  };
+  let braille_backend: Option<String> = if env.is_same_object(&braille_backend, &null).unwrap() {
+    None
+  } else {
+    Some(env.get_string(&braille_backend).unwrap().into())
+  };
+  let text: String = env.get_string(&text).unwrap().into();
+  let interrupt: bool = match interrupt {
+    JNI_FALSE => false,
+    _ => true,
+  };
+  output(
+    synthesizer.as_deref(),
+    voice.as_deref(),
+    language.as_deref(),
+    rate,
+    volume,
+    pitch,
+    braille_backend.as_deref(),
+    &text,
+    interrupt,
+  )
+  .unwrap();
 }
